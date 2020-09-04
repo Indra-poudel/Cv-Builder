67d1bbcc03d03f5806f022b10fbb4f9d24de48af
Nitesh Raut <rautesh6@gmail.com>
2020-09-04 21:10:14 +0545

    CV-3: Code refactor

--- src/router.jsx
+++ src/router.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const FRUITS = ['Apple', 'Banana', 'Mango', 'Orange'];
 
 const Router = () => {
-  FRUITS.map((fruit) => <div>{fruit}</div>);
+  FRUITS.map((fruit) => <div key={fruit}>{fruit}</div>);
 };
 
 export default Router;
