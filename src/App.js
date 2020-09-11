4e325cebcb25ed13fe2d86690c080fea258163ef
subash <iambruce1010@gmail.com>
2020-09-11 23:46:15 +0545

    setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-
-import Router from './router';
+// import Router from './router';
+import SideInput from 'components/SideInput/SideInput';
+import InnerSideContent from 'components/SideInput/InnerSideContent';
 
 const App = () => (
   <React.Fragment>
-    <Router />
+    {/* <Router /> */}
+    <InnerSideContent />
+    <SideInput />
   </React.Fragment>
 );
 
---
+++ src/components/SideInput/SideInput.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SideInput() {
+  return (
+    <div>
+      <h1>This is my side bar</h1>
+    </div>
+  );
+}
+
+export default SideInput;
